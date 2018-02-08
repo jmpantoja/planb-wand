e685e49756e970ed0337d2520598e9193fd7b739
jmpantoja <jmpantoja@gmail.com>
2018-02-08 10:43:09 +0100

    Task #1, Primer commit

---
+++ tests/acceptance/_bootstrap.php
@@ -0,0 +1,4 @@
+<?php
+$base = codecept_root_dir();
+
+include $base . '/vendor/autoload.php'; // composer autoload
---
+++ tests/unit/_bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+$base = codecept_root_dir();
+
+include $base . '/vendor/autoload.php'; // composer autoload
+
+$kernel = \AspectMock\Kernel::getInstance();
+$kernel->init([
+    'debug' => true,
+    'cacheDir' => $base . 'var/mocks',
+    'includePaths' => [
+        $base . '/src',
+        $base . '/vendor',
+    ],
+    'excludePaths' => [
+        $base . "/vendor/behat",
+        $base . "/vendor/codeception",
+        $base . "/vendor/mockery",
+        $base . "/vendor/goaop",
+        $base . "/vendor/nikic",
+        $base . "/vendor/phpunit",
+        $base . "/vendor/symfony/symfony/src/Symfony/Component/VarDumper"
+    ],
+]);
